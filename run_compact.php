7492a85fb823117af806fa82451a3495f3648571
Maksym Semenykhin <MaksymSemenykhin@users.noreply.github.com>
2017-09-04 10:12:33 +0300

    Update run_compact.php

--- run_compact.php
+++ run_compact.php
@@ -11,7 +11,7 @@ $excluded_dbs = [
 ];
 
 //https://github.com/MaksymSemenykhin/php-HTTPRequester
-include 'HTTPRequester/HTTPRequester.php';
+include 'HTTPRequester.php';
 
 define(HTTPRVERBOSE,true);
 
