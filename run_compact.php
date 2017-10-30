0a5a00c65d73a3e104f79814741b24e880d0bde5
ikorzh <31621462+ikorzh@users.noreply.github.com>
2017-10-30 12:33:59 +0200

    Add PUT method fixes

--- run_compact.php
+++ run_compact.php
@@ -33,7 +33,7 @@ foreach($all_dbs as $count => $db){
 
 	echo " ==> $db #$count\n";
 	
-        print_r(HTTPRequester::HTTPput ($db_host."/$db/_revs_limit",[$revs_limit],['Content-Type: application/json']) );	
+        print_r(HTTPRequester::HTTPput ($db_host."/$db/_revs_limit",$revs_limit) );	
 	print_r(HTTPRequester::HTTPPost($db_host."/$db/_compact"   ,[]           ,['Content-Type: application/json']) );	
 	
 	
